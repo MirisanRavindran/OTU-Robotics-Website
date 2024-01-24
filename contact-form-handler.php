9a569b06d827f0e6153bc026d1a5679602cfd7b9
Matthew <matthewlee031205@gmail.com>
2024-01-23 20:38:41 -0500

    Fixes

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -5,7 +5,7 @@
     $message = $_POST['message'];
 
 
-    $email_from = 'info@easytutorialspro.com';
+    $email_from = 'mirisan.ravindran@ontarioechu.net';
 
     $email_subject = "New Form Submission";
     
