9c772c668eb010e902be121de4eacde4a5654d57
Matthew <matthewlee031205@gmail.com>
2024-01-24 05:59:53 -0500

    Added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -116,3 +116,12 @@ const testimonials = [
           `;
   };
   window.onload = displayTestimonial;
+
+
+  function scrollSmoothTo(elementId) {
+    var element = document.getElementById(elementId);
+    element.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth'
+    });
+  }
